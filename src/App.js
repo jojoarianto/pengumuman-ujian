61650fa5be73faff1ac948eebfe5f420997b0f97
Joko Irianto <irianto94@outlook.com>
2018-09-15 03:13:07 +0700

    add material-ui and index page done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
+// import library material component
+// install with "npm install @material-ui/core"
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 class App extends Component {
+  state = {
+    nopes: ''
+  }
+
+  // handle change of nopes input
+  handleChange = nopes => event => {
+    this.setState({
+      [nopes]: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+          <AppBar position="static" color="primary" className="App-bar">
+            <Toolbar variant="dense">
+              <Typography variant="title" color="inherit">
+              Sistem Pengumuman
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <br/> <br/> 
+
+          <TextField
+            id="nopes"
+            label="Nomer Peserta"
+            className="Nopes"
+            value={this.state.nopes}
+            onChange={this.handleChange('nopes')}
+            margin="normal"
+          />
+          <Button variant="contained" color="primary" size="large">
+            Lihat Hasil
+          </Button>
+
+          <p className="footer-msg">Develope with love at <a href="https://olimpiade.id">Olimpiade.id</a></p>
       </div>
     );
   }
