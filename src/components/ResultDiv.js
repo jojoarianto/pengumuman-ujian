c085d641f01203b341b0e095967c6e2b05871ee7
Joko Irianto <irianto94@outlook.com>
2018-09-17 23:59:36 +0700

    component result div

---
+++ src/components/ResultDiv.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ResultDiv extend React.Component {
+	// render result div
+	render () {
+		return (
+			<div>
+				<h3>Hasil Pencarian</h3>
+
+				<p>Nama : MrX</p>
+				<p>Kelas : Y</p>
+			</div>
+		)
+	}
+
+}
+
+export default ResultDiv;
