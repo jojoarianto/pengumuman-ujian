99bb85937f23c10a636451436f168516bf38f951
Joko Irianto <irianto94@outlook.com>
2018-10-04 21:10:41 +0700

    adding select event

--- src/components/NopesForm.js
+++ src/components/NopesForm.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
 
 class NopesForm extends React.Component {
 	// data nopes
 	state = {
-		nopes: ''
+		nopes: '',
+		age: '',
+	    name: 'hai'
 	}
 
 	// handle change of nopes input
@@ -16,22 +20,42 @@ class NopesForm extends React.Component {
 	    });
 	};
 
+	// handle changet of select event
+	handleChangeEvent = event => {
+	    this.setState({ [event.target.name]: event.target.value });
+	};
+
 	// render nopes form component
 	render() {
 		return (
 			<div>
-			<TextField
-				id="nopes"
-				label="Nomer Peserta"
-				className="Nopes"
-				value={this.state.nopes}
-				onChange={this.handleChange('nopes')}
-				margin="normal"
-			/>
-			<br/>
-			<Button color="primary" variant="outlined" size="large">
-				Cari Peserta
-		    </Button>
+				<InputLabel htmlFor="age-simple">Event Name</InputLabel>
+				<Select
+		            value={this.state.age}
+		            onChange={this.handleChangeEvent}
+		            inputProps={{
+		              name: 'age',
+		              id: 'age-simple',
+		            }}
+		            margin="normal"
+		         >
+		            <MenuItem value={10}>Ten</MenuItem>
+		            <MenuItem value={20}>Twenty</MenuItem>
+		            <MenuItem value={30}>Thirty</MenuItem>
+		        </Select>
+				<br/>
+				<TextField
+					id="nopes"
+					label="Nomer Peserta"
+					className="Nopes"
+					value={this.state.nopes}
+					onChange={this.handleChange('nopes')}
+					margin="normal"
+				/>
+				<br/>
+				<Button color="primary" variant="outlined" size="large">
+					Cari Peserta
+			    </Button>
 			</div>
 		)
 	}
